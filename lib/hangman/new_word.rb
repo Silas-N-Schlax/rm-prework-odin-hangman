e87c0c369fba7167757c430aba5c0300369d0ad9
SilasNS <silasschlax@gmail.com>
2026-04-15 16:54:19 -0400

    Create NewWord class to generate random word.
    NewWord class picks word greater then 4 chars in length but less then
    13 chars in length for the random hangman word the player must try to
    guess.

---
+++ lib/hangman/new_word.rb
@@ -0,0 +1,20 @@
+# Opens words.txt, and picks 1 word that is between 5-12 chars long.
+class NewWord
+  attr_reader :word
+
+  def initalize
+    @word = random_word
+  end
+
+  def random_word
+    words = load_words
+    word = words.sample
+    word = random_word unless word.length > 4 && word.length < 13
+    word
+  end
+
+  def load_words
+    file_path = File.join(__dir__, "words.txt")
+    File.read(file_path).split("\n")
+  end
+end

e32e96cba68f220a765498ddf8071ec0cc37cf89
SilasNS <silasschlax@gmail.com>
2026-04-15 20:04:09 -0400

    Updated #load_game method and minor changes

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,17 +1,24 @@
 require "colorize"
+require_relative "hangman/play"
 # Hangman Game
 class Hangman
+  def initialize
+    @play = Play.new
+  end
+
   def start
-    print "Would you like to load a saved game? (y/n) -> ".colorize(:blue)
+    print "Would you like to load a saved game? (y/n) -> ".colorize(:yellow)
     key = gets.chomp.downcase.chars.first
     key == "y" ? load_game : new_game
   end
 
   def load_game
-    puts "load game"
+    @play.load_game
+    puts "Game Over!".colorize(:cyan)
   end
 
   def new_game
-    puts "new game"
+    @play.play
+    puts "Game Over!".colorize(:cyan)
   end
 end
